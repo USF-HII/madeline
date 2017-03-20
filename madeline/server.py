dcd599d078a097d38154f990412e7f3c660630b2
Kevin Counts <counts@digicat.org>
2017-03-20 10:47:49 -0400

    Remove rate limiting (moved to branch ratelimit)

--- madeline/server.py
+++ madeline/server.py
@@ -7,52 +7,6 @@ import time
 import flask
 import jinja2
 
-from functools import update_wrapper
-from flask import request, g
-from redis import Redis
-
-redis = Redis(host='redis')
-rate_request = os.environ.get('RATE_REQUEST', 1000)
-rate_time = os.environ.get('RATE_TIME', 60 * 5)
-
-class RateLimit(object):
-    expiration_window = 10
-
-    def __init__(self, key_prefix, limit, per, send_x_headers):
-        self.reset = (int(time.time()) // per) * per + per
-        self.key = key_prefix + str(self.reset)
-        self.limit = limit
-        self.per = per
-        self.send_x_headers = send_x_headers
-        p = redis.pipeline()
-        p.incr(self.key)
-        p.expireat(self.key, self.reset + self.expiration_window)
-        self.current = min(p.execute()[0], limit)
-
-    remaining = property(lambda x: x.limit - x.current)
-    over_limit = property(lambda x: x.current >= x.limit)
-
-def get_view_rate_limit():
-    return getattr(g, '_view_rate_limit', None)
-
-def on_over_limit(limit):
-    return 'You hit the rate limit', 429
-
-def ratelimit(limit, per=300, send_x_headers=True,
-              over_limit=on_over_limit,
-              scope_func=lambda: request.remote_addr,
-              key_func=lambda: request.endpoint):
-    def decorator(f):
-        def rate_limited(*args, **kwargs):
-            key = 'rate-limit/%s/%s/' % (key_func(), scope_func())
-            rlimit = RateLimit(key, limit, per, send_x_headers)
-            g._view_rate_limit = rlimit
-            if over_limit is not None and rlimit.over_limit:
-                return over_limit(rlimit)
-            return f(*args, **kwargs)
-        return update_wrapper(rate_limited, f)
-    return decorator
-
 def cli(cmd_args, timeout=None):
     result = {}
     decode_method = 'utf-8'
@@ -104,14 +58,12 @@ def route_version():
     return cli(['cat', 'madeline-version.txt'])['output']
 
 @app.route("/submit", methods=['POST'])
-@ratelimit(limit=rate_request, per=rate_time)
 def route_run():
     job_id = str(uuid.uuid4())
 
     work_dir = 'tmp/{job_id}'.format(job_id=job_id)
     data_file = os.path.join(work_dir,'input.txt')
     output_prefix = os.path.join(work_dir, 'output')
-
 
     os.makedirs(work_dir)
 
